a7ec14e2d2150183494328fd1e63ff7224c77c6c
StormOtx <stormsnaps@gmail.com>
2022-10-09 07:02:45 +0200

    chore: Update of .gitignore and small margin change in combat ui

--- src/routes/gamemode/tutorial/combat/+page.svelte
+++ src/routes/gamemode/tutorial/combat/+page.svelte
@@ -40,13 +40,13 @@
     {#if cardsDrawn}
         <div class="absolute w-full h-full bg-black/70 top-0 z-10">
             <div class="flex flex-grow flex-col sm:flex-row items-center justify-evenly h-full">
-                <div on:click={(e) => pickedCard(0)} class="cursor-pointer" in:fly|local="{{ y: 200, x: 800, duration: 1000 }}">
+                <div on:click={(e) => pickedCard(0)} class="cursor-pointer mx-2" in:fly|local="{{ y: 200, x: 800, duration: 1000 }}">
                     <CursedCard type="basic" />
                 </div>
-                <div on:click={(e) => pickedCard(1)} class="cursor-pointer" in:fly|local="{{ y: 200, x: 600, delay: 500, duration: 1000 }}">
+                <div on:click={(e) => pickedCard(1)} class="cursor-pointer mx-2" in:fly|local="{{ y: 200, x: 600, delay: 500, duration: 1000 }}">
                     <CursedCard type="basic" />
                 </div>
-                <div on:click={(e) => pickedCard(2)} class="cursor-pointer" in:fly|local="{{ y: 200, x: 100, delay: 1000, duration: 1000 }}">
+                <div on:click={(e) => pickedCard(2)} class="cursor-pointer mx-2" in:fly|local="{{ y: 200, x: 100, delay: 1000, duration: 1000 }}">
                     <CursedCard type="basic" />
                 </div>
             </div>
@@ -85,9 +85,9 @@
             {/if}
         </div>
     </div>
-    <div in:fly|local="{{ y: 200, duration: 1000 }}" class="flex flex-col w-full items-center space-y-2">
+    <div in:fly|local="{{ y: 200, duration: 1000 }}" class="flex flex-wrap w-full items-center space-y-2">
         <div class="flex-grow w-full border-t-2 border-slate-100"></div>
-        <div class="flex w-full space-x-4">
+        <div class="flex flex-wrap w-full space-x-4">
             <AdventurerStats />
             <div class="flex flex-col">
                 <div class="flex flex-col">
