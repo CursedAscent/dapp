243d79963d47b9086e683bca1a982e2868a78a9c
StormOtx <stormsnaps@gmail.com>
2022-11-11 08:20:02 +0100

    style: Made homepage responsive, added cursed skull at the top left of the header for better navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 
     import { retrieveConnectedWallet, walletConnected } from "$lib/stores/starknet/stores.js";
 	  import { onMount } from "svelte";
+	import { page } from "$app/stores";
 
     onMount(async () => {
         // Auto mount wallet if connected
@@ -17,7 +18,12 @@
 </svelte:head>
 
 <div class="flex flex-col  bg-zinc-900 min-h-screen">
-  <div class="flex flex-row justify-end p-2">
+  <div class="flex flex-row justify-between p-2">
+    <a href="/">
+      {#if $page.url.pathname !== "/"}
+        <img src="images/cursed_skull_fit.png" width="84px" alt="Cursed skull homepage link" />
+      {/if}
+    </a>
     <div class="flex flex-row items-center rounded-full border-slate-100 bg-slate-100 p-2 cursor-pointer">
         {#if $walletConnected}
           <div class="border-green-500 bg-green-500 rounded-full w-4 h-4 mr-2"></div>
@@ -30,6 +36,6 @@
   </div>
   <slot />
   <div class="flex flex-row justify-center">
-    <p class="text-slate-400">Built by <a href="https://github.com/StormOtx">StormOtx</a> and <a href="https://github.com/bipbipbopbop">BipBop</a></p>
+    <p class="text-slate-400 text-center">Built by <a href="https://github.com/StormOtx">StormOtx</a> and <a href="https://github.com/bipbipbopbop">BipBop</a></p>
   </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,10 +19,10 @@
 </style>
 
 <div class="relative flex flex-col flex-grow items-center justify-center">
-    <div class="flex flex-col items-center max-w-5xl">
-        <img class="absolute max-w-sm skull-logo-animate" src="images/cursed_skull.png" alt="Cursed Ascent skull logo" />
-        <img class="mt-8 max-w-sm" src="images/cursed_pentagram.png" alt="Cursed Ascent pentagram logo">
-        <img class="max-w-sm" src="images/cursed_title.png" alt="Cursed Ascent logo title">
+    <div class="flex flex-col items-center max-w-5xl p-2">
+        <img class="absolute sm:max-w-sm skull-logo-animate" src="images/cursed_skull.png" alt="Cursed Ascent skull logo" />
+        <img class="mt-8 sm:max-w-sm" src="images/cursed_pentagram.png" alt="Cursed Ascent pentagram logo">
+        <img class="sm:max-w-sm" src="images/cursed_title.png" alt="Cursed Ascent logo title">
         <h1 class="text-slate-100 text-2xl text-center mt-8">A decentralized deckbuilding roguelike game based on Realms: Adventurers and running on Starknet.<br/> Built during MatchboxDAO Hackathon #2.</h1>
         <div class="mt-8">
             <div on:click={e => connectUserWallet()}>
